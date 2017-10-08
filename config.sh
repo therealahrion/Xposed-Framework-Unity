277b7864661548ddce1c2e9f03ca58c95075d342
therealahrion <ahrionmgallegos@gmail.com>
2017-10-08 15:01:20 -0600

    add config.sh permissions

--- config.sh
+++ config.sh
@@ -81,7 +81,12 @@ set_permissions() {
   test "$MAGISK" == "true" && set_perm_recursive $MODPATH 0 0 0755 0644 
  
   # CUSTOM PERMISSIONS
-  
+  set_perm_recursive $UNITY$SYS/bin 0 2000 0755 0755
+  set_perm $UNITY$SYS/bin/app_process32 0 2000 0755 u:object_r:zygote_exec:s0
+  set_perm $UNITY$SYS/bin/dex2oat 0 2000 0755 u:object_r:dex2oat_exec:s0
+  set_perm $UNITY$SYS/bin/patchoat 0 2000 0755 u:object_r:dex2oat_exec:s0
+  $IS64BIT && set_perm $UNITY$SYS/bin/app_process64 0 2000 0755 u:object_r:zygote_exec:s0
+
   # Some templates if you have no idea what to do:
   # Note that all files/folders have the $UNITY prefix - keep this prefix on all of your files/folders
   # Also note the lack of '/' between variables - preceding slashes are already included in the variables
