3aba536980517e0a2a3b0670cd80d0bdca3a4309
Zackptg5 <zackptg5@comcast.net>
2017-10-09 16:00:13 -0400

    Finally got it working
    Needed to patch app_process with linux sed binary, android sed sucks nads

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -9,15 +9,16 @@ TIMEOFEXEC=2
 
 ui_print "    Installing libs for $API API $ARCH device"
 if [ "$MAGISK" == true ]; then
-  for app_process in $INSTALLER/custom/$API/$ARCH/bin/app_process*; do
-    sed -i 's/\/system\/xposed.prop\x0/\/xposed.prop\x0\x0\x0\x0\x0\x0\x0\x0/g' $app_process
-  done
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY/xposed.prop
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process32_magisk $UNITY$SYS/bin/app_process32
+  $IS64BIT && $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process64_magisk $UNITY$SYS/bin/app_process64
 else
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY$SYS/xposed.prop
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process32 $UNITY$SYS/bin/app_process32
+  $IS64BIT && $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process64 $UNITY$SYS/bin/app_process64
   test $API -ge 22 && find $SYS $VEN -type f -name '*.odex.gz' 2>/dev/null | while read f; do mv "$f" "$f.xposed"; done
 fi
 
-$CP_PRFX $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY/xposed.prop
-$CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process32 $UNITY$SYS/bin/app_process32
 $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/dex2oat $UNITY$SYS/bin/dex2oat
 $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/oatdump $UNITY$SYS/bin/oatdump
 $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/patchoat $UNITY$SYS/bin/patchoat
@@ -26,7 +27,6 @@ $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libart-compiler.so $UNITY$SYS/lib/liba
 $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libsigchain.so $UNITY$SYS/lib/libsigchain.so
 $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libxposed_art.so $UNITY$SYS/lib/libxposed_art.so
 if $IS64BIT; then
-  $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process64 $UNITY$SYS/bin/app_process64
   $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart.so $UNITY$SYS/lib64/libart.so
   $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart-compiler.so $UNITY$SYS/lib64/libart-compiler.so
   $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart-disassembler.so $UNITY$SYS/lib64/libart-disassembler.so
