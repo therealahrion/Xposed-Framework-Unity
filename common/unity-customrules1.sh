a2067795b112621b70b77371903d8f028395af15
Zackptg5 <zackptg5@comcast.net>
2017-10-12 10:41:00 -0400

    Rollback to 88.0 since 88.1 is bugged

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -1,60 +1,28 @@
 TIMEOFEXEC=2
 
-if [ $API -ne 25 ] && [ $API -ne 24 ]; then
-  if [ "$MAGISK" == true ]; then
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY/xposed.prop
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process32_magisk $UNITY$SYS/bin/app_process32
-    $IS64BIT && $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process64_magisk $UNITY$SYS/bin/app_process64
-  else
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY$SYS/xposed.prop
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process32 $UNITY$SYS/bin/app_process32
-    $IS64BIT && $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process64 $UNITY$SYS/bin/app_process64
-    test $API -ge 22 && find $SYS $VEN -type f -name '*.odex.gz' 2>/dev/null | while read f; do mv "$f" "$f.xposed"; done
-  fi
-  $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/dex2oat $UNITY$SYS/bin/dex2oat
-  $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/oatdump $UNITY$SYS/bin/oatdump
-  $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/patchoat $UNITY$SYS/bin/patchoat
-  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libart.so $UNITY$SYS/lib/libart.so
-  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libart-compiler.so $UNITY$SYS/lib/libart-compiler.so
-  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libsigchain.so $UNITY$SYS/lib/libsigchain.so
-  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libxposed_art.so $UNITY$SYS/lib/libxposed_art.so
-  if $IS64BIT; then
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart.so $UNITY$SYS/lib64/libart.so
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart-compiler.so $UNITY$SYS/lib64/libart-compiler.so
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart-disassembler.so $UNITY$SYS/lib64/libart-disassembler.so
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libsigchain.so $UNITY$SYS/lib64/libsigchain.so
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libxposed_art.so $UNITY$SYS/lib64/libxposed_art.so
-  else
-    $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libart-disassembler.so $UNITY$SYS/lib/libart-disassembler.so
-  fi
+if [ "$MAGISK" == true ]; then
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY/xposed.prop
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process32_magisk $UNITY$SYS/bin/app_process32
+  $IS64BIT && $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process64_magisk $UNITY$SYS/bin/app_process64
+ else
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY$SYS/xposed.prop
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process32 $UNITY$SYS/bin/app_process32
+  $IS64BIT && $CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/app_process64 $UNITY$SYS/bin/app_process64
+  test $API -ge 22 && find $SYS $VEN -type f -name '*.odex.gz' 2>/dev/null | while read f; do mv "$f" "$f.xposed"; done
+fi
+$CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/dex2oat $UNITY$SYS/bin/dex2oat
+$CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/oatdump $UNITY$SYS/bin/oatdump
+$CP_PRFX $INSTALLER/custom/$API/$ARCH/bin/patchoat $UNITY$SYS/bin/patchoat
+$CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libart.so $UNITY$SYS/lib/libart.so
+$CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libart-compiler.so $UNITY$SYS/lib/libart-compiler.so
+$CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libsigchain.so $UNITY$SYS/lib/libsigchain.so
+$CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libxposed_art.so $UNITY$SYS/lib/libxposed_art.so
+if $IS64BIT; then
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart.so $UNITY$SYS/lib64/libart.so
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart-compiler.so $UNITY$SYS/lib64/libart-compiler.so
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libart-disassembler.so $UNITY$SYS/lib64/libart-disassembler.so
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libsigchain.so $UNITY$SYS/lib64/libsigchain.so
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib64/libxposed_art.so $UNITY$SYS/lib64/libxposed_art.so
 else
-  if [ "$MAGISK" == true ]; then
-    VER=88.0
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/xposed.prop $UNITY/xposed.prop
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/bin/app_process32_magisk $UNITY$SYS/bin/app_process32
-    $IS64BIT && $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/bin/app_process64_magisk $UNITY$SYS/bin/app_process64
-  else
-    VER=88.1
-	$CP_NBPRFX $INSTALLER/custom/$API/$VER/XposedBridge.jar $UNITY$SYS/framework/XposedBridge.jar
-    $CP_NBPRFX $INSTALLER/custom/$API/$VER/module.prop $INSTALLER/module.prop
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/xposed.prop $UNITY$SYS/xposed.prop
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/bin/app_process32 $UNITY$SYS/bin/app_process32
-    $IS64BIT && $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/bin/app_process64 $UNITY$SYS/bin/app_process64
-  fi
-  $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/bin/dex2oat $UNITY$SYS/bin/dex2oat
-  $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/bin/oatdump $UNITY$SYS/bin/oatdump
-  $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/bin/patchoat $UNITY$SYS/bin/patchoat
-  $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib/libart.so $UNITY$SYS/lib/libart.so
-  $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib/libart-compiler.so $UNITY$SYS/lib/libart-compiler.so
-  $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib/libsigchain.so $UNITY$SYS/lib/libsigchain.so
-  $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib/libxposed_art.so $UNITY$SYS/lib/libxposed_art.so
-  if $IS64BIT; then
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib64/libart.so $UNITY$SYS/lib64/libart.so
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib64/libart-compiler.so $UNITY$SYS/lib64/libart-compiler.so
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib64/libart-disassembler.so $UNITY$SYS/lib64/libart-disassembler.so
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib64/libsigchain.so $UNITY$SYS/lib64/libsigchain.so
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib64/libxposed_art.so $UNITY$SYS/lib64/libxposed_art.so
-  else
-    $CP_PRFX $INSTALLER/custom/$API/$VER/$ARCH/lib/libart-disassembler.so $UNITY$SYS/lib/libart-disassembler.so
-  fi
+  $CP_PRFX $INSTALLER/custom/$API/$ARCH/lib/libart-disassembler.so $UNITY$SYS/lib/libart-disassembler.so
 fi
