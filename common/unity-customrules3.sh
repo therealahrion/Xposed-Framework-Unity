8c7a329ae9e02361d8747b4e0bd30b88a672769e
Zackptg5 <zackptg5@comcast.net>
2017-10-31 11:29:57 -0400

    remove existing xposed magisk modules

--- common/unity-customrules3.sh
+++ common/unity-customrules3.sh
@@ -3,3 +3,8 @@ TIMEOFEXEC=1
 if [ -f "/data/app/de.robv.android.xposed.installer/de.robv.android.xposed.installer.apk" ]; then
   test "$(cmp /data/app/de.robv.android.xposed.installer/de.robv.android.xposed.installer.apk $INSTALLER/system/app/XposedFramework/XposedFramework.apk)" && $WP_PRFX FOL/data/data/de.robv.android.xposed.installer
 fi
+test -d /magisk/xposed21 && rm -rf /magisk/xposed21
+test -d /magisk/xposed22 && rm -rf /magisk/xposed22
+test -d /magisk/xposed23 && rm -rf /magisk/xposed23
+test -d /magisk/xposed24 && rm -rf /magisk/xposed24
+test -d /magisk/xposed25 && rm -rf /magisk/xposed25
--- common/unity-files-wipe.sh
+++ common/unity-files-wipe.sh
@@ -1,6 +1,1 @@
 FOL/data/app/de.robv.android.xposed.installer
-FOL/magisk/xposed21
-FOL/magisk/xposed22
-FOL/magisk/xposed23
-FOL/magisk/xposed24
-FOL/magisk/xposed25
