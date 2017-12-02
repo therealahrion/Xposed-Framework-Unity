f4bbfb4d503d9146a1475f0bd32fb8ff62ad1e8a
Zackptg5 <zackptg5@comcast.net>
2017-12-02 13:45:45 -0500

    Bug fixes

--- common/unity-audmodlib/audmodlib-post-fs-data.sh
+++ common/unity-audmodlib/audmodlib-post-fs-data.sh
@@ -17,10 +17,10 @@ MIXS="${MIXS} $(find -L $SYS -type f -name "*mixer_paths*.xml")"
 
 # SEPOLICY SETTING FUNCTION
 set_sepolicy() {
-  if [ $(basename $SEINJECT) == "sepolicy-inject" ]; then
-	test -z $2 && $SEINJECT -Z $1 -l || $SEINJECT -s $1 -t $2 -c $3 -p $4 -l
+  if [ "$(basename $SEINJECT)" == "sepolicy-inject" ]; then
+	if [ -z $2 ]; then $SEINJECT -Z $1 -l; else $SEINJECT -s $1 -t $2 -c $3 -p $4 -l; fi
   else
-    test -z $2 && $SEINJECT --live "permissive $(echo $1 | sed 's/,/ /g')" || $SEINJECT --live "allow $1 $2 $3 { $(echo $4 | sed 's/,/ /g') }" 
+    if [ -z $2 ]; then $SEINJECT --live "permissive $(echo $1 | sed 's/,/ /g')"; else $SEINJECT --live "allow $1 $2 $3 { $(echo $4 | sed 's/,/ /g') }"; fi
   fi
 }
 
@@ -30,7 +30,8 @@ set_sepolicy mediaserver mediaserver_tmpfs file read,write,execute
 set_sepolicy mediaserver system_file file execmod
 set_sepolicy $SOURCE init unix_stream_socket connectto
 set_sepolicy $SOURCE property_socket sock_file getattr,open,read,write,execute
-set_sepolicy $SOURCE,audio_prop
+set_sepolicy $SOURCE
+set_sepolicy audio_prop
 
 # MOD PATCHES
 
