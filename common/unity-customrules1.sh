889232731902967907767380bb20136e0c4fdceb
Zackptg5 <zackptg5@comcast.net>
2018-01-11 11:36:35 -0500

    Add test apk for oreo

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -22,10 +22,11 @@ else
   rm -f $INSTALLER/system/bin/app_process32_magisk
   $IS64BIT && rm -f $INSTALLER/system/bin/app_process64_magisk
   POSTFSDATA=false
-  test $API -ge 22 && find $SYS $VEN -type f -name '*.odex.gz' 2>/dev/null | while read f; do mv "$f" "$f.xposed"; done
+  test $API -ge 22 && find -L $SYS -type f -name '*.odex.gz' 2>/dev/null | while read f; do mv "$f" "$f.xposed"; done
 fi
 
 if $OREONEW; then
-  cp_ch $INSTALLER/custom/$API/framework/XposedBridge.jar $INSTALLER/system/framework/XposedBridge.jar
+  cp_ch $INSTALLER/custom/XposedBridge.jar $INSTALLER/system/framework/XposedBridge.jar
+  ch_ch $INSTALLER/custom/XposedInstaller.apk $INSTALLER/system/app/XposedInstaller/XposedInstaller.apk
   sed -i "s/version=.*/version=v90-beta1/" $INSTALLER/module.prop
 fi
--- common/unity-customrules2.sh
+++ common/unity-customrules2.sh
@@ -1,6 +1,6 @@
 TIMEOFEXEC=4
 
 if [ "$MAGISK" == false ]; then
-  test $API -ge 22 && find $SYS $VEN -type f -name '*.odex.gz.xposed' 2>/dev/null | while read f; do mv "$f" "${f%.xposed}"; done
+  test $API -ge 22 && find -L $SYS -type f -name '*.odex.gz.xposed' 2>/dev/null | while read f; do mv "$f" "${f%.xposed}"; done
   set_permissions
 fi
